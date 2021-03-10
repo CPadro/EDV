ae5e6982d3df835c3b01a52fa962cfd000db9c3c
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-03-10 10:31:29 +0100

    fixed project

---
+++ Unity_Projects/Platformer_fixed/Assets/Scripts/Eject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eject : MonoBehaviour
+{
+    public float springForce = 30f;
+    private Animator _anim;
+
+    private void Awake()
+    {
+        _anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) {
+            PlatformerPlayer player = other.GetComponent<PlatformerPlayer>();
+            player.Jump(springForce);
+            _anim.SetTrigger("playerOn");
+        }
+    }
+}
---
+++ Unity_Projects/Platformer_fixed/Assets/Scripts/PlatformerPlayer.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformerPlayer : MonoBehaviour
+{
+    public float speed = 4.5f;
+    private Rigidbody2D _body;
+    private Animator _anim;
+
+    public float jumpForce = 10.0f;
+
+    //
+    private BoxCollider2D _box;
+
+    void Start()
+    {
+        _body = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _box = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        float deltaX = Input.GetAxisRaw("Horizontal") * speed;
+
+        _anim.SetFloat("speed", Mathf.Abs(deltaX));
+
+
+        Vector2 movement = new Vector2(deltaX, _body.velocity.y);
+        _body.velocity = movement;
+
+        //
+        Vector3 max = _box.bounds.max;
+        Vector3 min = _box.bounds.min;
+        Vector2 corner1 = new Vector2(max.x, min.y - .1f);
+        Vector2 corner2 = new Vector2(min.x, min.y - .2f);
+        Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+        bool grounded = (hit != null);
+
+        _anim.SetBool("jumping", !grounded);
+
+        //
+        _body.gravityScale = (grounded &&
+            Mathf.Approximately(deltaX, 0.0f) &&
+            Mathf.Abs(_body.velocity.y) < 0.1f) ? 0f : 1f;
+
+        if (grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            //_body.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            Jump(jumpForce);
+        }
+
+        //
+        MovingPlatform platform = null;
+        Vector3 pScale = Vector3.one;
+
+        if (hit != null)
+        {
+            platform = hit.GetComponent<MovingPlatform>();
+        }
+        if (platform != null)
+        {
+            transform.parent = platform.transform;
+            pScale = platform.transform.localScale;
+        }
+        else
+        {
+            transform.parent = null;
+        }
+
+        if (!Mathf.Approximately(deltaX, 0f))
+        {
+            transform.localScale = new Vector3(Mathf.Sign(deltaX) / pScale.x, 1f / pScale.y, 1f);
+        }
+
+    }
+
+    public void Jump(float force)
+    {
+        _body.AddForce(Vector2.up * force, ForceMode2D.Impulse);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (_box != null)
+        {
+            Vector3 max = _box.bounds.max;
+            Vector3 min = _box.bounds.min;
+            Vector3 corner1 = new Vector3(max.x, min.y - .1f, 0);
+            Vector3 corner2 = new Vector3(min.x, min.y - .2f, 0);
+            Collider2D hit = Physics2D.OverlapArea(corner1, corner2);
+
+            bool grounded = (hit != null);
+            if (grounded)
+                Gizmos.color = Color.red;
+            else
+                Gizmos.color = Color.green;
+
+            Gizmos.DrawCube((corner1 + corner2) * 0.5f, corner2 - corner1);
+        }
+    }
+
+}
