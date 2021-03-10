65af59fcc9c06c6909f3fa40a7228151f3d7f2b3
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-03-10 10:59:57 +0100

    added parallax scrolling, background layers and spring

---
+++ Unity_Projects/Platformer_fixed/Assets/Scripts/ParallaxHorizontal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxHorizontal : MonoBehaviour
+{
+    public float parallaxFraction;
+    private float startPosition;
+    private Transform cam;
+
+    void Start()
+    {
+        startPosition = transform.position.x;
+        cam = Camera.main.transform;
+    }
+
+    void LateUpdate()
+    {
+        float offset = (cam.position.x * parallaxFraction);
+        transform.position = new Vector3(startPosition + offset, transform.position.y, transform.position.z);
+    }
+}
