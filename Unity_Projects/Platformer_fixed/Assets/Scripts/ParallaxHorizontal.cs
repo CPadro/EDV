241544fba143e1014f4162ee7aad22da9ff4cdb5
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-03-15 19:08:57 +0100

    finished platformer

--- Unity_Projects/Platformer_fixed/Assets/Scripts/ParallaxHorizontal.cs
+++ Unity_Projects/Platformer_fixed/Assets/Scripts/ParallaxHorizontal.cs
@@ -8,15 +8,24 @@ public class ParallaxHorizontal : MonoBehaviour
     private float startPosition;
     private Transform cam;
 
+    // 
+    private float width;
+
     void Start()
     {
         startPosition = transform.position.x;
         cam = Camera.main.transform;
+        //
+        width = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     void LateUpdate()
     {
         float offset = (cam.position.x * parallaxFraction);
+        //
+        float moved = cam.position.x - offset;
+        if(moved > startPosition + width) startPosition += width;
+        else if(moved < startPosition - width) startPosition -= width;
         transform.position = new Vector3(startPosition + offset, transform.position.y, transform.position.z);
     }
 }
